4af2d7905806b13552076522376b6c2a45651ca3
LucasMull <muller.lucas@Hotmail.com>
2021-05-12 11:18:28 -0300

    fix: remove pre-compiled binaries and push working test code

--- test.c
+++ test.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <assert.h>
 
 #include "libpq-fe.h"
 
-void exit_nicely(PGconn *conn)
-{
-  PQfinish(conn);
-  exit(EXIT_SUCCESS);
-}
 
-int main(int argc, char *argv[]) {
+int main() 
+{
+  const char *conninfo = \
+      "host = tuffi.db.elephantsql.com "
+      "user = ffiuqzvz "
+      "password = M7AOD_TFnZSg70vROqaITY05gCRNtiw8 "
+      "dbname = ffiuqzvz";
+  fprintf(stderr, "%s\n", conninfo);
+
   /* Make a connection to the DB */
-  PGconn *conn = PQconnectdb("dbname = ababa");
+  PGconn *conn = PQconnectdb(conninfo);
 
   /* Check to see that the backend connection was succesfully made */
   if (CONNECTION_OK != PQstatus(conn)) {
     fprintf(stderr, "Connection to database failed: %s", \
         PQerrorMessage(conn));
-    exit_nicely(conn);
+    PQfinish(conn);
+    return EXIT_FAILURE;
   }
 
+  sleep(100); // check at elephantsql.com if connection is active
+
+  PQfinish(conn);
   return EXIT_SUCCESS;
 }
